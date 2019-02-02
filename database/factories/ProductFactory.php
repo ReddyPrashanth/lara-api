5cab659097d02590c0aef0d63a431661de168eb6
PrashanthSreepathi <34295243+PrashanthSreepathi@users.noreply.github.com>
2019-02-02 00:41:50 -0600

    database seeding for Product and Review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        "name" => $faker->word,
+        "price" => $faker->numberBetween(100, 1000),
+        "detail" => $faker->paragraph,
+        "stock" => $faker->randomDigit,
+        "discount" => $faker->numberBetween(2,30)
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,15 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Model\Product;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        "customer" => $faker->name,
+        "product_id" => function() {
+            return Product::all()->random();
+        },
+        "Review" => $faker->paragraph,
+        "rating" => $faker->numberBetween(0,5)
     ];
 });
